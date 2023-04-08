16cf50757cee0b623534dddb4485895391607b97
MZayed97 <mzayed97pro@gmail.com>
2023-04-09 00:55:30 +0200

    adding posts

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const dotenv = require('dotenv')
-const express = require('express')
-const mongoose = require("mongoose")
-const helmet = require('helmet')
-const morgan  = require('morgan')
-const app = express()
-const userRoute = require("./routes/users")
+const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const app = express();
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/post");
 
-dotenv.config()
+dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true},console.log("connected to mongo"));
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true },
+  console.log("connected to mongo")
+);
 
 //middleware
-app.use(express.json())
-app.use(helmet())
-app.use(morgan('common'))
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
 
-app.use("/users" , userRoute)
+app.use("/users", userRoute);
+app.use("/posts", postRoute);
 
-app.listen(8080,()=> console.log("running on http://localhost:8080"))
+app.listen(8080, () => console.log("running on http://localhost:8080"));
